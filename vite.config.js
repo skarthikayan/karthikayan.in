13cb5cb49deb043fae2b31912155aaa51c9b880c
Karthikayan <skarthikayan7@gmail.com>
2024-07-30 22:51:03 +0530

    Feature: added seo optimization tweaks

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,7 @@ export default defineConfig({
   plugins: [
     react(),
     svgr({
-      svgrOptions: {},
-      esbuildOptions: {},
       include: "**/*.svg?react",
-      exclude: "",
     }),
   ],
 });
